b0122d95322a975a69cd20d694c15990d0035720
BWD-ThasinurMalek-2425 <thasinur.malek@student.odisee.be>
2026-05-20 16:22:21 +0200

    Add BMI calculation view

--- bmi_project/bmi_app/views.py
+++ bmi_project/bmi_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def bmi_berekenen(request):
+    resultaat = None
+    categorie = None
+
+    if request.method == 'POST':
+        gewicht = float(request.POST['gewicht'])
+        lengte = float(request.POST['lengte'])
+        
+        bmi = gewicht / (lengte ** 2)
+        bmi = round(bmi, 2)
+        
+        if bmi < 18.5:
+            categorie = "Ondergewicht"
+        elif bmi < 25:
+            categorie = "Normaal gewicht"
+        elif bmi < 30:
+            categorie = "Overgewicht"
+        else:
+            categorie = "Obesitas"
+        
+        resultaat = bmi
+
+    return render(request, 'bmi.html', {
+        'resultaat': resultaat,
+        'categorie': categorie
+    })
